4af12ee4e36fd8f314ef67525324c9162706535a
Sourabh <37789151+Souravgodara@users.noreply.github.com>
2024-05-11 12:23:24 +0530

    Mobile Navbar Added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { ThemeToggle } from './ThemeToogle'
 import { Button } from './ui/button'
 import SignIn from './SignIn'
+import NavbarModal from './navbar-modal'
 
 export default function Navbar() {
   let categories = [
@@ -25,13 +26,16 @@ export default function Navbar() {
   ]
   return (
     <nav className='flex items-center justify-between'>
-      <h1 className='w-full px-2 text-center font-semibold md:w-fit md:text-left md:text-xl'>
+      <div className='ml-4 md:hidden'>
+        <NavbarModal categories={categories} />
+      </div>
+      <h1 className='w-full px-2 text-center text-xl font-semibold md:w-fit'>
         <span className='text-red-700'>FRESH </span>
         KHABAR
       </h1>
       <ul className=' hidden items-center justify-center gap-14 p-2 md:flex'>
-        {categories.map(category => (
-          <li key={category.link}>{category.name}</li>
+        {categories.map(c => (
+          <li key={c.link}>{c.name}</li>
         ))}
       </ul>
       <div className='hidden gap-3 md:flex'>
---
+++ src/components/navbar-modal.tsx
@@ -0,0 +1,34 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@/components/ui/sheet'
+import { RiMenu2Fill } from 'react-icons/ri'
+import { Button } from './ui/button'
+
+export default function NavbarModal({ categories }) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <RiMenu2Fill />
+      </SheetTrigger>
+      <SheetContent className=' w-56' side={'left'}>
+        <SheetHeader>
+          <SheetTitle>Fresh Khabar</SheetTitle>
+        </SheetHeader>
+        <div className='mt-10 flex flex-col items-center gap-2'>
+          {categories.map(c => (
+            <li className='list-none ' key={c.link}>
+              <Button className='text-base' variant='ghost'>
+                {c.name}
+              </Button>
+            </li>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
