be113f557fac0a423705279b2acc31570dedb9b2
Sourabh <37789151+Souravgodara@users.noreply.github.com>
2024-06-12 14:07:03 +0530

    UI Improved

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,8 +6,8 @@ import { FaYoutube } from 'react-icons/fa'
 
 export default function Footer() {
   return (
-    <footer className='flex flex-col items-center justify-center gap-3 text-gray-700'>
-      <div className='flex items-center justify-center gap-6'>
+    <footer className=' mt-14 flex flex-col items-center justify-center gap-3 bg-zinc-300 p-4 text-gray-700'>
+      <div className=' mt-3 flex items-center justify-center gap-6'>
         <Link href={'/about'}>About us</Link>
         <Link href={'/about'}>Contact</Link>
         <Link href={'/about'}>Jobs</Link>
@@ -17,7 +17,7 @@ export default function Footer() {
         <FaYoutube size={28} />
         <FaFacebook size={28} />
       </div>
-      <aside>
+      <aside className='p-4'>
         <p>Copyright © 2024 - All right reserved by Fresh Khabar Ltd</p>
       </aside>
     </footer>
